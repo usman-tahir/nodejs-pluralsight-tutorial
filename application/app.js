933b21c860a2520add68ab16f08c8a5534c022d7
usman-tahir <tahir.usman.ali94@outlook.com>
2017-01-17 10:50:57 -0500

    added two basic routes served through express; added 'npm start' as a command to run the server

--- application/app.js
+++ application/app.js
@@ -2,6 +2,20 @@
 (function () {
   
   "use strict";
-  var express = require("express");
+  var express = require("express"),
+    app = express(),
+    port = 5000;
+  
+  app.get("/", function (request, response) {
+    response.send("Hello, World!");
+  });
+  
+  app.get("/books", function (request, response) {
+    response.send("Hello, Books!");
+  });
+  
+  app.listen(5000, function (error) {
+    console.log("Running server on port " + port);
+  });
   
 }());
