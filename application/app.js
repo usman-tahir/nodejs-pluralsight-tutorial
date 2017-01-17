eddc9a1ab3d41a9bdfd38b38f3336f03f9667577
usman-tahir <tahir.usman.ali94@outlook.com>
2017-01-17 17:17:53 -0500

    set up nodemon for app serving; process.env.PORT bug remains (switching ports when restarting)

--- application/app.js
+++ application/app.js
@@ -1,25 +1,21 @@
 
-(function () {
-  
-  'use strict';
-  var express = require('express'),
-    app = express(),
-    port = 5000;
-  
-  app.use(express.static('public'));
-  app.use(express.static('src/views'));
-  
-  
-  app.get('/', function (request, response) {
-    response.send('Hello, World!');
-  });
-  
-  app.get('/books', function (request, response) {
-    response.send('Hello, Books!');
-  });
-  
-  app.listen(5000, function (error) {
-    console.log('Running server on port ' + port);
-  });
-  
-}());
+console.log('process.env.PORT: ' + process.env.PORT);
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+app.get('/', function (request, response) {
+  response.send('Hello, World!');
+});
+
+app.get('/books', function (request, response) {
+  response.send('Hello, Books!');
+});
+
+app.listen(port, function (error) {
+  console.log('Running server on port ' + port);
+});
--- application/gulpfile.js
+++ application/gulpfile.js
@@ -1,39 +1,50 @@
 
-(function () {
-  'use strict';
-  
-  var gulp = require('gulp'),
-    jsHint = require('gulp-jshint'),
-    jscs = require('gulp-jscs'),
-    jsFiles = ['*.js', 'src/**/*.js'];
-  
-  gulp.task('style', function () {
-    return gulp.src(jsFiles)
-      .pipe(jsHint())
-      .pipe(jsHint.reporter('jshint-stylish', {
-        verbose: true
-      }))
-      .pipe(jscs());
-  });
-  
-  gulp.task('inject', function () {
-    var wiredep = require('wiredep').stream,
-      inject = require('gulp-inject'),
-      injectSource = gulp.src(['./public/css/*.css', './public/js/*.js'], {
-        read: false
-      }),
-      injectOptions = {
-        ignorePath: '/public'
-      },
-      options = {
-        bowerJson: require('./bower.json'),
-        directory: './public/lib',
-        ignorePath: '../../public'
-      };
-    return gulp.src('./src/views/*.html')
-      .pipe(wiredep(options))
-      .pipe(inject(injectSource, injectOptions))
-      .pipe(gulp.dest('./src/views'));
-  });
-  
-}());
+var gulp = require('gulp'),
+  jsHint = require('gulp-jshint'),
+  jscs = require('gulp-jscs'),
+  nodemon = require('gulp-nodemon'),
+  jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('style', function () {
+  return gulp.src(jsFiles)
+    .pipe(jsHint())
+    .pipe(jsHint.reporter('jshint-stylish', {
+      verbose: true
+    }))
+    .pipe(jscs());
+});
+
+gulp.task('inject', function () {
+  var wiredep = require('wiredep').stream,
+    inject = require('gulp-inject'),
+    injectSource = gulp.src(['./public/css/*.css', './public/js/*.js'], {
+      read: false
+    }),
+    injectOptions = {
+      ignorePath: '/public'
+    },
+    options = {
+      bowerJson: require('./bower.json'),
+      directory: './public/lib',
+      ignorePath: '../../public'
+    };
+  return gulp.src('./src/views/*.html')
+    .pipe(wiredep(options))
+    .pipe(inject(injectSource, injectOptions))
+    .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function () {
+  var options = {
+    script: 'app.js',
+    delayTime: 1,
+    env: {
+      PORT: 5000
+    },
+    watch: jsFiles
+  };
+  return nodemon(options)
+    .on('restart', function (event) {
+      console.log('Restarting');
+    });
+});
