85fb07388de94058d04ebb80fcab25b9ebc5aa1f
usman-tahir <tahir.usman.ali94@outlook.com>
2017-01-19 17:58:48 -0500

    created boilerplate code for the /Books and /Authors routes; implemented a simple /Books route

--- application/app.js
+++ application/app.js
@@ -3,21 +3,31 @@ console.log('process.env.PORT: ' + process.env.PORT);
 var express = require('express'),
   app = express(),
   handlebars = require('express-handlebars'),
+  bookRoute = express.Router(),
   port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 
-app.engine('.hbs', handlebars({extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.set('view engine', 'ejs');
+
+bookRoute.route('/')
+  .get(function(request, response) {
+    response.send('Book Directory');
+  });
+
+bookRoute.route('/single')
+  .get(function(request, response) {
+    response.send("Single book");
+  });
+app.use('/Books', bookRoute);
 
 app.get('/', function (request, response) {
-  var sampleList = ['a', 'b', 'c'];
-  response.render('index', {list: sampleList});
-});
-
-app.get('/books', function (request, response) {
-  response.send('Hello, Books!');
+  var routes = [
+    {Link: '/Books', Text: 'Books'},
+    {Link: '/Authors', Text: 'Authors'}
+  ];
+  response.render('index', {navigation: routes});
 });
 
 app.listen(process.env.PORT || port, function (error) {
